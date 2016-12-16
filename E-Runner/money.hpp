eebc33757ce3914b3a4367c6f6fe2cde3ca02a6d
appcrashwin7 <xcv678@wp.pl>
2016-12-16 23:13:57 +0100

    add money class also change move speed of player

---
+++ E-Runner/money.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <SFML/Graphics/RectangleShape.hpp>
+
+class money
+{
+	sf::RectangleShape moneyShape;
+
+public:
+	money();
+	money(sf::Vector2f pos);
+	~money();
+
+	sf::Vector2f getPosition();
+	sf::RectangleShape *getShape();
+};
